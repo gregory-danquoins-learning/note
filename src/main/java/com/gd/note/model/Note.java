6baae6f48c1014b4bb2cc1b32df98e18ee3dee08
Grégory Danquoins <152988641+Gregory-Danquoins@users.noreply.github.com>
2025-09-25 09:31:06 +0200

    add database connection

--- src/main/java/com/gd/note/model/Note.java
+++ src/main/java/com/gd/note/model/Note.java
@@ -3,6 +3,9 @@ package com.gd.note.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.Data;
 
 @Entity
--- src/main/java/com/gd/note/security/SecurityConfig.java
+++ src/main/java/com/gd/note/security/SecurityConfig.java
@@ -4,10 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -15,17 +21,48 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-                requests
-                        .requestMatchers("/contact").permitAll()
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/admin").denyAll()
-                        .requestMatchers("/admin/**").denyAll()
-                        .anyRequest().authenticated());
+        // http.authorizeHttpRequests((requests) ->
+        //         requests
+        //                 .requestMatchers("/contact").permitAll()
+        //                 .requestMatchers("/public/**").permitAll()
+        //                 .requestMatchers("/admin").denyAll()
+        //                 .requestMatchers("/admin/**").denyAll()
+        //                 .anyRequest().authenticated());
         // http.formLogin(withDefaults());
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // http.csrf(csrf -> csrf.disable());
+
+        // http.sessionManagement(session ->
+        //         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        http.authorizeHttpRequests((requests) ->
+                requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager manager =
+                new JdbcUserDetailsManager(dataSource);
+        if (!manager.userExists("uu")) {
+            manager.createUser(
+                    User.withUsername("uu")
+                            .password("{noop}uu")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("aa")) {
+            manager.createUser(
+                    User.withUsername("aa")
+                            .password("{noop}aa")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }
